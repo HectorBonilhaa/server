06cbdf5b71213505d29c66512f482516c381c74f
HectorBonilhaa <hectoroliveira1@gmail.com>
2023-02-18 00:50:40 -0300

    deploy

--- src/server.ts
+++ src/server.ts
@@ -7,14 +7,14 @@ const app = Fastify()
 app.register(cors)
 app.register(appRoutes)
 
-// app.listen({
-//   port: 3333,
-//   host: 'https://server-production-6a90.up.railway.app/',
-// }).then(() => {
-//   console.log('HTTP Server running!')
-// })
-
-app.listen(process.env.PORT || 5000, () => {
-  console.log("Server is Running");
-  
+app.listen({
+  port: 5454,
+  host: 'https://server-production-6a90.up.railway.app/',
+}).then(() => {
+  console.log('HTTP Server running!')
 })
+
+// app.listen(process.env.PORT || 5000, () => {
+//   console.log("Server is Running");
+  
+// })
